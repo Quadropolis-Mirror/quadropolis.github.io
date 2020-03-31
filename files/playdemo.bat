c71c054d58bb4164b73be693aeb5e953ab4d2ae3
SalatielSauer <tiel_chaves@hotmail.com>
2020-03-31 16:05:02 -0300

    Add 500/1.000 files

---
+++ files/playdemo.bat
@@ -0,0 +1,10 @@
+REM This batch file is intended as the handler for demo files
+REM Associate it with .dmo files using Folder Options in Control Panel
+REM Double-clicking on a .dmo or asking your browser to open one
+REM will then launch Sauerbraten and play it.
+
+cd %~p0
+sauerbraten.bat -x"demo ""%~p1%~n1"""
+
+
+
