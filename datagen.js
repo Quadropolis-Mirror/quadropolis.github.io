d44eb1dca541940f39447a9329bbbe53f12bb7b3
SalatielSauer <tiel_chaves@hotmail.com>
2020-03-31 22:29:18 -0300

    Rename everything to lower-case

--- datagen.js
+++ datagen.js
@@ -8,7 +8,7 @@ post = JSON.parse(rawdata).reverse();
 function writeHtml(content) {curhtml += content + "\n";}
 function saveHtml(){fs.writeFileSync('index.html', curhtml);}
 
-imageformats = ["bmp", "jpg", "png", "JPEG", "JPG", "PNG", "gif", "GIF", "BMP"];
+imageformats = ["bmp", "jpg", "png", "gif"];
 //function getday(){return (new Date()).toLocaleDateString()};
 function readNode() {
 curhtml = ""
@@ -22,9 +22,9 @@ writeHtml('<span style="color: white">/</span> <a href="allnodes2.json" download
 			if (post[p]) {
 				writeHtml('<div class="content">');
 				if (post[p].title) {
-					writeHtml("<h2><span class='counter'>(" + post[p].id + ")</span><a id='" + post[p].id + "' target='_blank' href='http://quadropolis.us/node/" + post[p].id + "'>" + post[p].title + "</a></h2>");
+					writeHtml("<h2><a href='#" + post[p].id + "' class='counter'>(" + post[p].id + ")</a> <a id='" + post[p].id + "' target='_blank' href='http://quadropolis.us/node/" + post[p].id + "'>" + post[p].title + "</a></h2>");
 				} else {
-					writeHtml("<h2><span class='counter'>(" + post[p].id + ")</span> Untitled Post</h2>")
+					writeHtml("<h2><a href='#" + post[p].id + "' class='counter'>(" + post[p].id + ")</a> Untitled Post</h2>")
 				};
 
 				if (post[p].author) {
@@ -35,18 +35,18 @@ writeHtml('<span style="color: white">/</span> <a href="allnodes2.json" download
 
 				if (post[p].image) {
 					if (post[p].image.includes("quadropolis.us/files/")) {
-						imagename = post[p].image.substr(22).replace(/%20/g, "-");
+						imagename = post[p].image.substr(22).replace(/%20/g, "-").toLowerCase();
 					} else {
-						imagename = post[p].image
+						imagename = post[p].image;
 					};
-					writeHtml ('<img src="' + imagename + '" loading="lazy"/>');
+					writeHtml ('<img alt=" this screenshot was not originally available or there was a problem extracting it" src="' + imagename + '" loading="lazy"/>');
 				};
 				writeHtml('<p class="post">' + post[p].content.replace(/\n/g, "<br>") + "</p>")
 				writeHtml('<span style="color: #ff9800">Files:</span>')
 				if (post[p].hrefs.length > 0) {
 					for (u = 0; u < post[p].hrefs.length; u++) {
 						if (post[p].hrefs[u].includes("quadropolis.us/files/")) {
-							filename = post[p].hrefs[u].substr(22).replace(/%20/g, "-");
+							filename = post[p].hrefs[u].substr(22).replace(/%20/g, "-").toLowerCase();
 							writeHtml('<a href="' + filename + '" download>' + filename.substr(6) + '</a>');
 						} else {
 							writeHtml('<a href="' + post[p].hrefs[u] + '">' + post[p].hrefs[u] + "</a>");
